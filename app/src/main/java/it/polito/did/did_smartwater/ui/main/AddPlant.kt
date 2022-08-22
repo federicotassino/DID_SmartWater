53358ebca9674ded028c1524efe6a874d7dfa2d6
MSI\feder <federicotassino@gmail.com>
2022-08-22 18:12:17 +0200

    Aggiunti calendar e numberPicker

--- app/src/main/java/it/polito/did/did_smartwater/ui/main/AddPlant.kt
+++ app/src/main/java/it/polito/did/did_smartwater/ui/main/AddPlant.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
+import android.widget.*
+import androidx.annotation.NonNull
+import androidx.core.view.marginTop
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import it.polito.did.did_smartwater.R
@@ -61,8 +61,56 @@ class AddPlant : Fragment(R.layout.fragment_add_plant) {
         val buttonManual = view.findViewById<RadioButton>(R.id.buttonManual)
         val buttonScheduled = view.findViewById<RadioButton>(R.id.buttonScheduled)
         val buttonAutomatic = view.findViewById<RadioButton>(R.id.buttonAutomatic)
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+        val textViewNote = view.findViewById<TextView>(R.id.textViewNote)
+        val layoutParams = textViewNote.layoutParams as ViewGroup.MarginLayoutParams
+        calendarView.setVisibility(View.GONE)
+        layoutParams.setMargins(0, 40, 0, 0)
+        val textViewGiorni = view.findViewById<TextView>(R.id.textViewGiorni)
+        textViewGiorni.setVisibility(View.GONE)
+        val pickerDays = view.findViewById<NumberPicker>(R.id.pickerDays)
+        pickerDays.minValue = 1
+        pickerDays.maxValue = 30
+        pickerDays.setVisibility(View.GONE)
         val textPlantNote = view.findViewById<EditText>(R.id.plantNote)
         val buttonAdd = view.findViewById<Button>(R.id.buttonAdd)
+        val dateDebug = view.findViewById<TextView>(R.id.dateDebug)    //debug per data select
+
+        buttonScheduled.setOnClickListener(){
+            calendarView.setVisibility(View.VISIBLE)
+            layoutParams.setMargins(0, 1500, 0, 0)
+            dateDebug.text = "Seleziona data inizio"
+            textViewGiorni.setVisibility(View.VISIBLE)
+            pickerDays.setVisibility(View.VISIBLE)
+        }
+
+        buttonManual.setOnClickListener(){
+            calendarView.setVisibility(View.GONE)
+            layoutParams.setMargins(0, 40, 0, 0)
+            textViewGiorni.setVisibility(View.GONE)
+            pickerDays.setVisibility(View.GONE)
+        }
+
+        buttonAutomatic.setOnClickListener(){
+            calendarView.setVisibility(View.GONE)
+            layoutParams.setMargins(0, 40, 0, 0)
+            textViewGiorni.setVisibility(View.GONE)
+            pickerDays.setVisibility(View.GONE)
+        }
+
+        calendarView
+            .setOnDateChangeListener(
+                CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
+                    // In this Listener we are getting values
+                    // such as year, month and day of month
+                    // on below line we are creating a variable
+                    // in which we are adding all the cariables in it.
+                    val date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    // set this date in TextView for Display
+                    dateDebug.text = date
+                })
 
         buttonAdd.setOnClickListener(){
             //viewModel.plantsList = viewModel.plantsList.plus("\n").plus(textPlantName.text.toString())
