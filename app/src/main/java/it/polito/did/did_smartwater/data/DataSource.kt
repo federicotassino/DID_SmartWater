065150315b6b306499b0308e48265660c7224b5b
MSI\feder <federicotassino@gmail.com>
2022-08-23 18:16:37 +0200

    addPlant funzionante

--- app/src/main/java/it/polito/did/did_smartwater/data/DataSource.kt
+++ app/src/main/java/it/polito/did/did_smartwater/data/DataSource.kt
@@ -6,16 +6,11 @@ import it.polito.did.did_smartwater.R
 class DataSource {
     fun loadPlants(): List<Plant>{
         return listOf<Plant>(
-            Plant(R.string.plant1, "Basilico", ""),
-            Plant(R.string.plant2, "Basilico", ""),
-            Plant(R.string.plant3,"Basilico", ""),
-            Plant(R.string.plant4,"Basilico", ""),
-            Plant(R.string.plant5,"Basilico", ""),
-            Plant(R.string.plant6,"Basilico", ""),
-            Plant(R.string.plant7,"Basilico", ""),
-            Plant(R.string.plant8,"Basilico", ""),
-            Plant(R.string.plant9,"Basilico", ""),
-            Plant(R.string.plant10,"Basilico", ""),
+            Plant(R.string.plant1, "Basilico", 0, "23-08-22", 2, "note"),
+            Plant(R.string.plant2, "Basilico", 0, "23-08-22", 2, "note"),
+            Plant(R.string.plant3, "Basilico", 0, "23-08-22", 2, "note"),
+            Plant(R.string.plant4, "Basilico", 0, "23-08-22", 2, "note"),
+            Plant(R.string.plant5, "Basilico", 0, "23-08-22", 2, "note"),
         )
     }
 }
